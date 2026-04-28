ca31c744dd628382d24cfaa6690e4b266bafb5cf
Mateusz Urban <mateuszurban2@gmail.com>
2026-04-28 14:23:45 +0000

    Export notification rule models and service from domain package

--- src/weather_agent/domain/rules/__init__.py
+++ src/weather_agent/domain/rules/__init__.py
@@ -3,20 +3,26 @@
 from __future__ import annotations
 
 from weather_agent.domain.rules.models import (
+    CELValidationError,
     NotificationEvent,
     NotificationRule,
     RuleCreate,
+    RuleNotFoundError,
     RuleUpdate,
+    ShortIdCollisionError,
 )
 from weather_agent.domain.rules.service import NotificationRuleService
 from weather_agent.domain.rules.short_id_generator import generate_short_id, strip_hash_prefix
 
 __all__ = [
+    "CELValidationError",
     "NotificationEvent",
     "NotificationRule",
     "NotificationRuleService",
     "RuleCreate",
+    "RuleNotFoundError",
     "RuleUpdate",
+    "ShortIdCollisionError",
     "generate_short_id",
     "strip_hash_prefix",
 ]
